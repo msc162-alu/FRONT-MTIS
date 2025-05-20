2b77c5d156902d16b210f2c8942fc54e2f9158fb
mdmrk <mariodavo.20@gmail.com>
2025-05-20 13:11:55 +0100

    titulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { SensorFiltrosComponent } from './sensor-filtros/sensor-filtros.component';
 import { AboutComponent } from './about/about.component';
 import { ActasComponent } from './actas/actas.component';
+import { ExpedicionTituloComponent } from './expedicion-titulo/expedicion-titulo.component';
 
 export const routes: Routes = [
     {path: 'about', component: AboutComponent},
     {path: 'filtro', component: SensorFiltrosComponent},
     {path: 'actas', component: ActasComponent},
+    {path: 'expedicion-titulo', component: ExpedicionTituloComponent},
     {path: '', redirectTo: '/about', pathMatch: 'full'}
 ];
 
